99225892725e3a8a6b2c77b38946059a87ef5874
Remya Mary Paul <remyamarypaul@gmail.com>
2022-04-02 21:16:29 +0530

    updated marketing user pages

---
+++ src/main/java/testCases/Admin/TC_DeleteMarketingUser.java
@@ -0,0 +1,39 @@
+
+package testCases.Admin;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentTest;
+import pages.Admin.createMarketingUserPage;
+import wrappers.CustomerPortalWrappers;
+
+
+public class TC_DeleteMarketingUser extends CustomerPortalWrappers{
+	String environment = "";
+
+	@BeforeClass
+	public void setValues() throws InterruptedException {
+		dataSheetName = "ManageMarketingUser";
+		testCaseName  = "Delete Marketing User";
+		testDescription = "Delete marketing user and verify.";
+		category = "Regression Test";
+		authors = "Remya Mary Paul";
+		browserName = "chrome";
+		ExtentTest test = startTestCase(testCaseName, testDescription);
+		test.assignAuthor(authors);
+		test.assignCategory(category);
+	}
+
+	@Test(dataProvider = "fetchData")
+	public void editMarketingUser(String empCode, String name , String userRole, String editedRole) throws Exception {
+		boolean editFlag = true;
+		new createMarketingUserPage(driver,test)
+		.clickDeleteMarketingUserLink()
+		.clickdeleteConfirmButton()
+		.verifyDeleteMarketingEmployee(empCode);
+			
+	}
+	
+	
+}
+
